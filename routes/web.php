5c4af5de46b69890367c6c66450ae451405c5303
Gustavo Antonio De Oliveira <antonio.tec.gustavo@gmail.com>
2024-01-24 16:27:54 -0300

    Inicio do Projeto

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return 'home';
+// });
+
+
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function () {
+    return 'login';
+})->name('site.login');
+
+//app
+Route::prefix('/app')->group(function () {
+    Route::get('/clientes', function () {
+        return 'clientes';
+    })->name('app.clientes');
+    Route::get('/fornecedores', function () {
+        return 'fornecedores';
+    })->name('app.fornecedores');
+    Route::get('/produtos', function () {
+        return 'produtos';
+    })->name('app.produtos');
+});
+
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+
+Route::fallback(function () {
+    echo 'Página não encontrada. <a href="' . route('site.index') . '">Clique aqui</a> para voltar a tela inicial.';
+});
